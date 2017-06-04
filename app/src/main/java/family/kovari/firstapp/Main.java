f747c1b921aaf97bf1988ea6764047e6692b641b
kovarimarkofficial <kovarimarkofficial@gmail.com>
2017-06-05 01:42:24 +0200

    Init commit

--- app/src/main/java/family/kovari/firstapp/Main.java
+++ app/src/main/java/family/kovari/firstapp/Main.java
@@ -2,7 +2,14 @@ package family.kovari.firstapp;
 
 /**
  * Created by mark on 2017.06.05..
+ *
  */
 
 public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Its working yay :D ");
+    }
 }
